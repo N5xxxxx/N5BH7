1d0c9fb2634c5c6257ef5f8af46a8784917e7216
N5xxxxx <ddlh59939@gmail.com>
2026-02-27 20:58:48 +0300

    Update music.js

--- modules/music.js
+++ modules/music.js
@@ -1,1 +1,5 @@
-
+client.on("clientReady", () => {
+  const guild = client.guilds.cache.first();
+  const connection = getVoiceConnection(guild.id);
+  if (connection) connection.subscribe(player);
+});
