46aba2d67fae3e92e25c94f8f9ff52280c1141cc
N5xxxxx <ddlh59939@gmail.com>
2026-02-28 00:00:56 +0300

    Update voice.js

--- modules/voice.js
+++ modules/voice.js
@@ -1,62 +1,23 @@
-const { 
-  joinVoiceChannel, 
-  getVoiceConnection, 
-  VoiceConnectionStatus,
-  entersState 
-} = require('@discordjs/voice');
+const { joinVoiceChannel } = require("@discordjs/voice");
 
 const GUILD_ID = "1367976354104086629";
 const VOICE_CHANNEL_ID = "1401074295022817381";
 
 module.exports = (client) => {
 
-  async function connect() {
-    try {
-      const guild = await client.guilds.fetch(GUILD_ID);
-      const channel = await guild.channels.fetch(VOICE_CHANNEL_ID);
+  client.once("clientReady", async () => {
 
-      if (!channel) return;
+    const guild = await client.guilds.fetch(GUILD_ID);
+    const channel = await guild.channels.fetch(VOICE_CHANNEL_ID);
 
-      const existing = getVoiceConnection(guild.id);
-      if (existing) return;
+    joinVoiceChannel({
+      channelId: channel.id,
+      guildId: guild.id,
+      adapterCreator: guild.voiceAdapterCreator,
+      selfDeaf: true
+    });
 
-      const connection = joinVoiceChannel({
-        channelId: channel.id,
-        guildId: guild.id,
-        adapterCreator: guild.voiceAdapterCreator,
-        selfDeaf: true
-      });
-
-      console.log("🎧 Connected to voice channel");
-
-      connection.on(VoiceConnectionStatus.Disconnected, async () => {
-        console.log("⚠️ Disconnected... Reconnecting");
-        try {
-          await entersState(connection, VoiceConnectionStatus.Connecting, 5000);
-        } catch {
-          connection.destroy();
-          setTimeout(connect, 3000);
-        }
-      });
-
-    } catch (err) {
-      console.error("Voice error:", err);
-    }
-  }
-
-  // أول ما يشتغل
-  client.once("clientReady", () => {
-    connect();
-  });
-
-  // لو أحد نقله أو طرده
-  client.on("voiceStateUpdate", (oldState, newState) => {
-    if (!client.user) return;
-
-    if (oldState.id === client.user.id && !newState.channelId) {
-      console.log("🚨 Bot was kicked. Rejoining...");
-      setTimeout(connect, 2000);
-    }
+    console.log("🎧 Connected to voice channel");
   });
 
 };
