8a90fd60510d18c68b8df79e0a6ce2ce43119987
N5xxxxx <ddlh59939@gmail.com>
2026-03-01 01:58:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-process.on("unhandledRejection", (err) => console.error("UNHANDLED:", err));
-process.on("uncaughtException", (err) => console.error("UNCAUGHT:", err));
+process.on("unhandledRejection", console.error);
+process.on("uncaughtException", console.error);
 
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,      // مهم
-    GatewayIntentBits.GuildModeration,   // مهم
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildModeration,
     GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.DirectMessages
   ]
+});
+
+client.once("ready", () => {
+  console.log("BOT IS ONLINE");
 });
 
 require("./modules/voice")(client);
